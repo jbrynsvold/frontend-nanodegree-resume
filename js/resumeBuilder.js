5badd208e31a05433e929e7fc04c4c5ca4bff6a7
jbrynsvold <jbrynsvold@gmail.com>
2016-03-14 15:53:28 -0500

    Missing semicolon

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,4 @@
 var formattedName = HTMLheaderName.replace("%data%", "Jason Brynsvold");
-var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer")
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
 $("#header").append(formattedName);
 $("#header").prepend(formattedRole);
