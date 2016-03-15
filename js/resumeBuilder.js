876f31ec5a015dad201e537462d2e0b6668ca181
jbrynsvold <jbrynsvold@gmail.com>
2016-03-15 16:58:24 -0500

    Added bio and appended to main

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,15 @@
 var formattedName = HTMLheaderName.replace("%data%", "Jason Brynsvold");
 var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+var skills = ["HTML", "CSS", "Javascript", "Blogging"];
+var bio = {
+  "name": "Jason Brynsvold",
+  "role": "Web Developer",
+  "contact": "214-505-4000",
+  "photo": ,
+  "welcome": "Here I am welcoming you to my page",
+  "skills": skills
+};
+
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
-
+$("#main").append(bio);
